530ec2c34132a0a1d5f54185aa9765131a9e2c8b
XiuqingZhu <102733646+XiuqingZhu@users.noreply.github.com>
2024-09-08 21:03:19 +0800

    Update NephroTOXPred.py

--- NephroTOXPred.py
+++ NephroTOXPred.py
@@ -40,7 +40,7 @@ def generate_feature_vector(smiles, feature_order):
 st.image("./logo.png")
 st.write("---")
 st.write("**Supported by Zhu's ai-drug service. **")
-st.write("**If you have questions, please contact me at 2018760376@gzhmu.edu.cn **"
+st.write("**If you have questions, please contact me at 2018760376@gzhmu.edu.cn **")
 st.write("**The affilated brain hospital, Guangzhou Medical university. **")
 
 # Define feature names
