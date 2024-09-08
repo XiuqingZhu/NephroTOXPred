7dce8fb366778810a1ed8bb12bbaea09be1acbb0
XiuqingZhu <102733646+XiuqingZhu@users.noreply.github.com>
2024-09-08 12:45:06 +0800

    Update NephroTOXPred.py

--- NephroTOXPred.py
+++ NephroTOXPred.py
@@ -48,68 +48,76 @@ st.image("./logo.png")
 # Streamlit user interface
 st.title("Nephrotoxic Component Predictor")
 
+st.write("**Please enter a SMILE string for predicting nephrotoxic components.**")
+
 # Smiles: string input
 smiles = st.text_input("SMILE:", value="")
 
 if st.button("Predict"):
-
-    #调用函数生成特征向量
+    # Generate feature vector
     feature_vector = generate_feature_vector(smiles, feature_names)
-    features = np.array([feature_vector])
-
-    # Predict class and probabilities
-    predicted_class = model.predict(features)[0]
-    predicted_proba = model.predict_proba(features)[0]
-
-    # Display prediction results
-    st.write(f"**Predicted Class:** {predicted_class}")
-    st.write(f"**Prediction Probabilities:** {predicted_proba}")
-
-    # Generate advice based on prediction results
-    probability = predicted_proba[predicted_class] * 100
-
-    if predicted_class == 1:
-        advice = (
-            f"According to our model, the compound that you submitted has a high risk of nephrotoxicity. "
-            f"The model predicts that your probability of having nephrotoxicity is {probability:.2f}%. "
-            "While this is just an estimate, it suggests that the compound may be at significant risk. "
-            "I recommend that you consult a cardiologist as soon as possible for further evaluation and "
-            "to ensure you receive an accurate diagnosis and necessary treatment."
-        )
+    
+    if feature_vector is None:
+        st.write("**Invalid SMILES string. Please provide a correct SMILES notation.**")
     else:
-        advice = (
-            f"According to our model, the compound that you submitted has a low risk of nephrotoxicity. "
-            f"The model predicts that your probability of not having nephrotoxicity is {probability:.2f}%. "
-            "However, maintaining a healthy lifestyle is still very important. "
-            "I recommend regular check-ups to monitor your heart health, "
-            "and to seek medical advice promptly if you experience any symptoms."
+        features = np.array([feature_vector])
+
+        # Create molecular image
+        mol = Chem.MolFromSmiles(smiles)
+        img = Draw.MolToImage(mol)
+        st.write("**The structure of this compound.**")
+        st.image(img, caption='Molecular Structure')
+
+        # Predict class and probabilities
+        predicted_class = model.predict(features)[0]
+        predicted_proba = model.predict_proba(features)[0]
+
+        # Display prediction results
+        st.write(f"**Predicted Class:** {predicted_class}")
+        st.write(f"**Prediction Probabilities:** {predicted_proba}")
+
+        # Generate advice based on prediction results
+        probability = predicted_proba[predicted_class] * 100
+
+        if predicted_class == 1:
+            advice = (
+                f"According to our model, the compound you submitted poses a high risk of nephrotoxicity. "
+                f"The model predicts that your likelihood of experiencing nephrotoxicity is {probability:.2f}%. "
+                "While this is only an estimation, it indicates that the compound may be at a significant risk. "
+            )
+        else:
+            advice = (
+                f"According to our model, the compound you submitted has a low risk of nephrotoxicity. "
+                f"The model predicts that your likelihood of not experiencing nephrotoxicity is {probability:.2f}%. "
+            )
+
+        st.write(advice)
+
+        # Calculate SHAP values and display force plot    
+        explainer = shap.TreeExplainer(model)  
+        shap_values = explainer.shap_values(pd.DataFrame([feature_vector], columns=feature_names))
+
+        # Choose the index of the output you want to visualize
+        output_index = 0  # Adjust this index based on your specific needs
+
+        # Force plot for the specified output
+        expected_value = explainer.expected_value[output_index]
+
+        positive_color = "#FF8C69"
+        negative_color = "#B23AEE"
+
+        # Generate the force plot for the specified output
+        shap.force_plot(
+            expected_value,
+            shap_values[output_index],
+            feature_names=pd.DataFrame([feature_vector], columns=feature_names).columns,
+            matplotlib=True,
+            show=True,
+            plot_cmap=[positive_color, negative_color]
         )
 
-    st.write(advice)
+        # Save and display the image
+        plt.savefig("./shap_force_plot.png", bbox_inches='tight', dpi=1200)
 
-    # Calculate SHAP values and display force plot    
-    explainer = shap.TreeExplainer(model)  
-    shap_values = explainer.shap_values(pd.DataFrame([feature_vector], columns=feature_names))
-
-    # Choose the index of the output you want to visualize
-    output_index = 0  # Adjust this index based on your specific needs
-
-    # Force plot for the specified output
-    expected_value = explainer.expected_value[output_index]
-
-    positive_color = "#FF8C69"
-    negative_color = "#B23AEE"
-
-    # Generate the force plot for the specified output
-    shap.force_plot(
-        expected_value,
-        shap_values[output_index],
-        feature_names=pd.DataFrame([feature_vector], columns=feature_names).columns,
-        matplotlib=True,
-        show=True,
-        plot_cmap=[positive_color, negative_color]
-    )
-
-    # Save and display the image
-    plt.savefig("./shap_force_plot.png", bbox_inches='tight', dpi=1200)
-    st.image("./shap_force_plot.png")
+        st.write("**The generated SHAP force plot of this compound.**")
+        st.image("./shap_force_plot.png")
