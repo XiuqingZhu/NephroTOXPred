b6bfb8fa2566c928a8f5672d92041f717c3a5d85
XiuqingZhu <102733646+XiuqingZhu@users.noreply.github.com>
2024-09-08 21:16:18 +0800

    Update NephroTOXPred.py

--- NephroTOXPred.py
+++ NephroTOXPred.py
@@ -40,7 +40,6 @@ def generate_feature_vector(smiles, feature_order):
 st.image("./logo.png")
 
 st.write("Supported by Zhu's ai-drug service. If you have questions, please contact me at 2018760376@gzhmu.edu.cn, The affilated brain hospital, Guangzhou Medical university. ")
-st.write("---")
 
 # Define feature names
 feature_df = pd.read_csv('./features_for_ML.csv')
