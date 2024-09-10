8d08ba139e0a16a844b6c82f41507d685d85bbfd
XiuqingZhu <102733646+XiuqingZhu@users.noreply.github.com>
2024-09-10 12:11:30 +0800

    Update NephroTOXPred.py

--- NephroTOXPred.py
+++ NephroTOXPred.py
@@ -159,6 +159,8 @@ if st.button("Predict"):
             show=True,
             plot_cmap=[positive_color, negative_color]
         )
+
+        run_progress()
         
         # Save and display the image
         plt.savefig("./shap_force_plot.png", bbox_inches='tight', dpi=1200)
